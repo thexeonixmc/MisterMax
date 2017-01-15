b0e4bbc297f9592b409fa14c24959c97dc6056d5
thexeonixmc <thexeonixmc@gmail.com>
2017-01-15 16:29:27 +0400

    Всякая хуйня там, это для теста o_o

---
+++ RepositoryInfo.lua
@@ -0,0 +1,12 @@
+ do
+ 
+ print("github/TheXeonixMc");
+ print("------------------");
+ print("Тестовая репозитория");
+ print("от TheXeonixMc"); 
+ print("Мой ВК: http://vk.com/thexeonixmc");
+ 
+    end
+   end
+  end
+  
